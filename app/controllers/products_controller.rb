fbfb5de130803ba20f7e80ca71aaf1305ae81885
annayanushkevich <fishinmyhead@gmail.com>
2016-04-18 18:36:56 -0700

    with pictures

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,10 +19,13 @@ class ProductsController < ApplicationController
   def create
     new_product = Product.new(name:params[:name], price:params[:price],description:params[:description], image:params[:image])
     new_product.save
+    flash[:success] = "new kitten has been added >^_^<"
+    redirect_to "/products/#{new_product.id}"
   end
 
   def edit
     @product = Product.find_by(id:params[:id])
+
   end
 
   def update
@@ -32,11 +35,15 @@ class ProductsController < ApplicationController
     @product.description =params[:description]
     @product.image =params[:image]
     @product.save
+    flash[:success] = "kitten information updated"
+    redirect_to "/products/#{@product.id}"
   end
 
   def destroy
     @product = Product.find_by(id:params[:id])
     @product.destroy
+    flash[:danger] = "your kitten has been destroyed"
+    redirect_to "/products"
   end
 
 end
